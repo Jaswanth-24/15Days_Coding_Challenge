51df9eb45bf2a6ef3f382b38fa0fdbd6040a4fbb
Jaswanth Sai Surakanti <147471834+Jaswanth-24@users.noreply.github.com>
2025-08-06 20:52:49 +0530

    Add files via upload

---
+++ Day 5/BinarySearch.java
@@ -0,0 +1,24 @@
+class BinarySearch {
+    public int search(int[] nums, int target) {
+        int low=0,high=nums.length-1;
+        if(nums.length<2){
+            if(target==nums[0]){
+                return 0;
+            }
+            return -1;
+        }
+        while(low<=high){
+            int mid=(low+high)/2;
+            if(nums[mid]==target){
+                return mid;
+            }
+            else if(nums[mid]>target){
+                high=mid-1;
+            }
+            else{
+                low=mid+1;
+            }
+        }
+        return -1;
+    }
+}
